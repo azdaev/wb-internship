be2dd289b8f8fd6ffaadf80377b117d0d2ab2476
amadeousss <amady833@gmail.com>
2022-11-20 15:18:34 +0600

    visitor completed

--- l2/wbschool_exam_L2/pattern/03_visitor.go
+++ l2/wbschool_exam_L2/pattern/03_visitor.go
@@ -1,7 +1,112 @@
 package main
 
+import "fmt"
+
 /*
-	Реализовать паттерн «посетитель».
-Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
-	https://en.wikipedia.org/wiki/Visitor_pattern
+	Посетитель.
+	Вид: Структурный.
+	Суть паттерна - добавление нового фунцкионала в программу, не изменяя классы, надо которыми
+	эти операции совершаются.
+
+	+: упрощает добавление операций, работающих со сложными структурами объектов
+	+: объединяет родственные операции в одном классе
+	-: паттерн не оправдан, если иерархия элементов часто меняется
+	-: может привести к нарушению инкапсуляции элементов
 */
+
+type Visitor interface {
+	visitForSquare(*Square)
+	visitForCircle(*Circle)
+	visitForRectangle(*Rectangle)
+}
+
+type Shape interface {
+	getType() string
+	accept(Visitor)
+}
+
+type Square struct {
+	side int
+}
+
+func (s *Square) accept(v Visitor) {
+	v.visitForSquare(s)
+}
+
+func (s *Square) getType() string {
+	return "Square"
+}
+
+type Circle struct {
+	radius int
+}
+
+func (c *Circle) accept(v Visitor) {
+	v.visitForCircle(c)
+}
+
+func (c *Circle) getType() string {
+	return "Circle"
+}
+
+type Rectangle struct {
+	l int
+	b int
+}
+
+func (t *Rectangle) accept(v Visitor) {
+	v.visitForRectangle(t)
+}
+
+func (t *Rectangle) getType() string {
+	return "rectangle"
+}
+
+type AreaCalculator struct {
+	area int
+}
+
+func (a *AreaCalculator) visitForSquare(s *Square) {
+	fmt.Println("Calculating area for square")
+}
+
+func (a *AreaCalculator) visitForCircle(s *Circle) {
+	fmt.Println("Calculating area for circle")
+}
+func (a *AreaCalculator) visitForRectangle(s *Rectangle) {
+	fmt.Println("Calculating area for rectangle")
+}
+
+type MiddleCoordinates struct {
+	x int
+	y int
+}
+
+func (a *MiddleCoordinates) visitForSquare(s *Square) {
+	fmt.Println("Calculating middle point coordinates for square")
+}
+
+func (a *MiddleCoordinates) visitForCircle(c *Circle) {
+	fmt.Println("Calculating middle point coordinates for circle")
+}
+func (a *MiddleCoordinates) visitForRectangle(t *Rectangle) {
+	fmt.Println("Calculating middle point coordinates for rectangle")
+}
+
+func main() {
+	square := &Square{side: 2}
+	circle := &Circle{radius: 3}
+	rectangle := &Rectangle{l: 2, b: 3}
+
+	areaCalculator := &AreaCalculator{}
+
+	square.accept(areaCalculator)
+	circle.accept(areaCalculator)
+	rectangle.accept(areaCalculator)
+
+	fmt.Println()
+	middleCoordinates := &MiddleCoordinates{}
+	square.accept(middleCoordinates)
+	circle.accept(middleCoordinates)
+	rectangle.accept(middleCoordinates)
+}
